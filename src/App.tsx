266c75536ce6e3d5f412fc30bc85947b7695495f
Phil Zona <phil.zona@vercel.com>
2025-04-09 21:57:10 -0400

    Better default setup for illustrative purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import Dot from "./components/dot/dot";
 import Svg from "./components/svg/svg";
+import { grid as g } from "./utils";
+
+const corners = [0.3, 0.7];
 
 function App() {
   return (
     <main>
       <div className="grid">
         <Svg>
-          <Dot x={320} y={320} />
+          {/* Change these */}
+          <Dot x={g(corners[0])} y={g(corners[0])} />
+          <Dot x={g(corners[0])} y={g(corners[1])} />
+          <Dot x={g(corners[1])} y={g(corners[0])} />
+          <Dot x={g(corners[1])} y={g(corners[1])} />
         </Svg>
       </div>
     </main>
---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+const GRID_SIZE = 640;
+
+export const grid = (f: number): number => {
+  return GRID_SIZE * f;
+};
