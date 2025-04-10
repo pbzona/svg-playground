dc4ac031e454b9bcb4b9ba763b0f7301077b548e
Phil Zona <phil.zona@vercel.com>
2025-04-09 21:23:35 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="grid">
+        <p>place your images here</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
